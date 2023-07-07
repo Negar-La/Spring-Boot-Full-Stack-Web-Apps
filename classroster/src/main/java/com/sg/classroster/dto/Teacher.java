4863f70b2727e8d10578f6dcc85d47be77f2767f
Negar Lashgari <negar.lashgari@gmail.com>
2023-07-07 10:27:17 -0400

    JDBC template and Thymeleaf

---
+++ classroster/src/main/java/com/sg/classroster/dto/Teacher.java
@@ -0,0 +1,62 @@
+package com.sg.classroster.dto;
+
+public class Teacher {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String specialty;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Teacher teacher = (Teacher) o;
+
+        if (id != teacher.id) return false;
+        if (!firstName.equals(teacher.firstName)) return false;
+        if (!lastName.equals(teacher.lastName)) return false;
+        return specialty.equals(teacher.specialty);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + specialty.hashCode();
+        return result;
+    }
+}
