4051fa8a345f276490571518e7afd740c65fb43d
Negar Lashgari <negar.lashgari@gmail.com>
2023-07-12 11:31:27 -0400

    entities completed

--- SchoolJPA/src/main/java/com/wiley/schoolJPA/entity/Student.java
+++ SchoolJPA/src/main/java/com/wiley/schoolJPA/entity/Student.java
@@ -1,4 +1,52 @@
 package com.wiley.schoolJPA.entity;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "student")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sid", nullable = false)
+    private Integer id;
+
+    @Column(name = "fname")
+    private String fName;
+
+    @Column(name = "lname")
+    private String lName;
+
+    @ManyToMany(mappedBy = "studentRoster")
+    private Set<Course> myClasses;
+
+    public Student() {}
+
+    public Student(Set<Course> myClasses) {
+        this.myClasses = myClasses;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
 }
--- SchoolJPA/src/main/java/com/wiley/schoolJPA/entity/Teacher.java
+++ SchoolJPA/src/main/java/com/wiley/schoolJPA/entity/Teacher.java
@@ -1,4 +1,65 @@
 package com.wiley.schoolJPA.entity;
 
-public interface Teacher {
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "teacher")
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tid", nullable = false)
+    private Integer id;
+
+    @Column(name = "tfname")
+    private String fName;
+
+    @Column(name = "tlname")
+    private String lName;
+
+    @Column(name = "dept")
+    private String dept;
+
+    @OneToMany(mappedBy = "courseTeacher")
+    private Set<Course> teacherClasses;
+
+    public Teacher() {
+    }
+
+    //added constructor for teacherClasses
+    public Teacher(Set<Course> teacherClasses) {
+        this.teacherClasses = teacherClasses;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
 }
