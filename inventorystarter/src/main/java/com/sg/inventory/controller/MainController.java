2a9ffe851ddf357457f6cad7bf0adbebcfdb424b
Negar Lashgari <negar.lashgari@gmail.com>
2023-07-17 11:54:58 -0400

    repositories and updating controller

--- inventorystarter/src/main/java/com/sg/inventory/controller/MainController.java
+++ inventorystarter/src/main/java/com/sg/inventory/controller/MainController.java
@@ -4,10 +4,17 @@ import com.sg.inventory.entities.Product;
 import com.sg.inventory.entities.Store;
 import com.sg.inventory.entities.Supplier;
 import javax.servlet.http.HttpServletRequest;
+
+import com.sg.inventory.repositories.ProductRepository;
+import com.sg.inventory.repositories.StoreRepository;
+import com.sg.inventory.repositories.SupplierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 /**
  *
@@ -15,78 +22,109 @@ import org.springframework.web.bind.annotation.PostMapping;
  */
 @Controller
 public class MainController {
+
+    @Autowired
+    ProductRepository products;
+
+    @Autowired
+    StoreRepository stores;
+
+    @Autowired
+    SupplierRepository suppliers;
     
     @GetMapping("/")
     public String index(Model model) {
-//        model.addAttribute("stores", null);
+        model.addAttribute("stores", stores.findAll());
         return "index";
     }
     
     @PostMapping("/addStore")
     public String addStore(Store store) {
-
+        stores.save(store);
         return "redirect:/";
     }
     
     @GetMapping("/viewInventory")
     public String viewInventory(Integer id, Model model) {
-        
-//        model.addAttribute("store", null);
-//        model.addAttribute("products", null);
-        
+
+        Store store = stores.findById(id).orElse(null);
+        List<Product> productList = products.findByStore(store);
+
+        model.addAttribute("store", store);
+        model.addAttribute("products", productList);
+
         return "inventory";
     }
     
     @PostMapping("/addProduct")
     public String addProduct(Product product, HttpServletRequest request) {
         int storeId = Integer.parseInt(request.getParameter("storeId"));
+        Store store = stores.findById(storeId).orElse(null);
+        product.setStore(store);
 
+        products.save(product);
         return "redirect:/viewInventory?id=" + storeId;
     }
     
     @GetMapping("/deleteProduct")
     public String deleteProduct(Integer id, Integer storeId) {
-
+        products.deleteById(id);
         return "redirect:/viewInventory?id=" + storeId;
     }
     
     @GetMapping("/product")
     public String displayProduct(Integer id, Model model) {
+        Product product = products.findById(id).orElse(null);
+        List<Supplier> supplierList = suppliers.findAll();
 
-        
-//        model.addAttribute("product", null);
-//        model.addAttribute("suppliers", null);
-        
+        model.addAttribute("product", product);
+        model.addAttribute("suppliers", supplierList);
+
         return "product";
     }
     
     @PostMapping("/addSupplier")
     public String addSupplier(Supplier supplier, HttpServletRequest request) {
         int productId = Integer.parseInt(request.getParameter("productId"));
+        Product product = products.findById(productId).orElse(null);
 
-        
+        supplier = suppliers.save(supplier);
+
+        product.getSuppliers().add(supplier);
+        products.save(product);
+
         return "redirect:/product?id=" + productId;
     }
     
     @PostMapping("/addExistingSupplier")
     public String addExistingSupplier(Integer productId, Integer supplierId) {
+        Product product = products.findById(productId).orElse(null);
+        Supplier supplier = suppliers.findById(supplierId).orElse(null);
 
-        
+        product.getSuppliers().add(supplier);
+        products.save(product);
+
         return "redirect:/product?id=" + productId;
     }
     
     @GetMapping("/removeSupplier")
     public String removeSupplier(Integer productId, Integer supplierId) {
+        Product product = products.findById(productId).orElse(null);
+        Supplier supplier = suppliers.findById(supplierId).orElse(null);
 
-        
+        product.getSuppliers().remove(supplier);
+        products.save(product);
+
         return "redirect:/product?id=" + productId;
     }
     
     @GetMapping("/supplier")
     public String displaySupplier(Integer id, Model model) {
-        
-//        model.addAttribute("supplier", null);
-        
+
+        Supplier supplier = suppliers.findById(id).orElse(null);
+
+        model.addAttribute("supplier", supplier);
+
         return "supplier";
     }
 }
---
+++ inventorystarter/src/main/java/com/sg/inventory/repositories/SupplierRepository.java
@@ -0,0 +1,10 @@
+package com.sg.inventory.repositories;
+
+import com.sg.inventory.entities.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
+
+}
